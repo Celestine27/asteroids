e3bdcdddd7f3719268ea52baefd01bb6116ae9b9
Celestine27 <223501507+Celestine27@users.noreply.github.com>
2025-08-13 12:06:15 +0200

    Added player rotation

--- main.py
+++ main.py
@@ -8,13 +8,16 @@ def main():
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
+        
         pygame.display.flip()
         
         # Limit the framerate to 60 fps
